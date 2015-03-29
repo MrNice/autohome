6cd851f83fe23e0cf870976a64eef491b43e39cb
Nicholas van de Walle <nicholas@niceguys.co>
2015-03-29 15:58:00 -0700

    v0.0.1 light working

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var ws = require('nodejs-websocket');
 
-require('tesselate')(['relay-mono'], function(tessel, modules) {
+require('tesselate')(['relay-mono'], function(tessel, m) {
+  console.log('turning on relay 1');
+  m.relay.turnOn(1);
+
   var connection = ws.connect('ws://10.0.0.24:1337', function() {
     connection.sendText('Tessel Up');
   });
